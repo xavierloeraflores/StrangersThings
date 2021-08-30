60c33044e5e1f868c2cf6dcd479cd770a40282f3
Xavier Loera Flores <x.loera.tech@gmail.com>
2021-08-29 19:46:13 -0700

    changed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,11 @@ const Footer = () => {
 
     return (
         <div id='footer'>
-            <span>Created By Xavier Loera Flores</span>
+            <img src='https://d2pyzn5nn2wsnc.cloudfront.net/sjsu-logo.svg?mtime=20210201070945&focal=none' alt='SJSU' />
+            <div>
+                <span>Xavier Loera Flores</span>
+            </div>
+            <img src='https://d2pyzn5nn2wsnc.cloudfront.net/powered-sjsu-blue.svg?mtime=20210202071951&focal=none' alt="Fullstack Academy" />
         </div>
     )
 }
